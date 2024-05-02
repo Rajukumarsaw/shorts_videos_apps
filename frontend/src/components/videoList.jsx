fe607dc0f02fff71bb6881d881337ad28bc02a30
Raju Kumar <rk8271740@gmail.com>
2024-05-03 00:26:16 +0530

    minor update

--- frontend/src/components/videoList.jsx
+++ frontend/src/components/videoList.jsx
@@ -1,21 +1,31 @@
-
 import Video from "./video";
-import { useState,useEffect  } from "react";
-const VideoList = ({videoData}) => {
-  const [videoDetail, setVideoDetail]=useState([]);
-  useEffect(()=>{
-    setVideoDetail(videoData.data); 
-  }, [videoData]);
-  return (
-    <>
-      <div className=" bg-[#000000cd] overflow-hidden">
-    <div className="relative flex flex-col items-center h-screen overflow-scroll snap-y snap-mandatory top-10">
-       {videoDetail && videoDetail.map((item)=>(<Video key={item._id} item={item}/>))}
-    </div>
-    </div>
-    </>
-  )
-}
+import { useState, useEffect } from "react";
 
-export default VideoList
+const VideoList = ({ videoData }) => {
+  const [videoDetail, setVideoDetail] = useState([]);
+
+  useEffect(() => {
+    if (videoData && videoData.data) {
+      setVideoDetail(videoData.data);
+    }
+  }, [videoData]);
+
+  return (
+    <div className="bg-[#000000cd] overflow-hidden">
+      <div className="relative flex flex-col items-center h-screen overflow-scroll snap-y snap-mandatory top-10">
+        {videoDetail.length > 0 ? (
+          videoDetail.map((item) => <Video key={item._id} item={item} />)
+        ) : (
+          Array.from({ length: 1 }).map((_, index) => (
+            <div key={index} className="w-[350px] h-[600px] animate-pulse bg-[#a8b0b9c8] my-8"></div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default VideoList;
+
+
 
