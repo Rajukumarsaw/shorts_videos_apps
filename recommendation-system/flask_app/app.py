30624ffe2a2e959bff949cdf7d6298d04c83393f
Raju Kumar <rk8271740@gmail.com>
2024-07-01 04:06:32 +0530

    default port added

--- recommendation-system/flask_app/app.py
+++ recommendation-system/flask_app/app.py
@@ -72,6 +72,8 @@ def recommend():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to port 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
