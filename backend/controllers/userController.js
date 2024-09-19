ab3cb34853cac764c24f78f23ed59def554173a4
Raju Kumar <rk8271740@gmail.com>
2024-09-20 02:09:46 +0530

    hashed password before saving to DB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,47 +1,60 @@
-const mongoose=require('mongoose');
-const User=require('../model/userModel');
-
+const mongoose = require('mongoose');
+const User = require('../model/userModel');
+const bcrypt = require('bcrypt');
 
 const userSignUp = async (req, res) => {
 	try {
-		const { email } = req.body;
-		const {userName}=req.body;
+		const { email, userName, password } = req.body;
 
 		const userEmail = await User.findOne({ email });
 		const user = await User.findOne({ userName });
 
-		
 		if (userEmail) {
 			return res.send({ message: "Email id already register.", alert: false });
 		}
-		 if(user){
-			return res.send({ message: "userName already exist, try other", alert: false });
+		if (user) {
+			return res.send({ message: "userName already exist, try other", alert: false });c
 		}
-		await User.create(req.body);
+
+		// Hash the password
+		const saltRounds = 10;
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+		// Create user with hashed password
+		await User.create({ ...req.body, password: hashedPassword });
 		res.send({ message: "Successfully sign up", alert: true });
 	} catch (error) {
 		res.status(500).send({ message: error.message });
 	}
 };
 
-
 const userLogin = async (req, res) => { 
 	try {
-		const { email } = req.body;
+		const { email, password } = req.body;
 		const user = await User.findOne({ email });
 
 		if (user) {
-			const userData = {
-				_id: user._id,
-				userName: user.userName,
-				email: user.email,
-			};
+			// Compare the provided password with the stored hashed password
+			const isPasswordValid = await bcrypt.compare(password, user.password);
 
-			res.send({
-				message: "Successfully Logged In",
-				alert: true,
-				data: userData,
-			});
+			if (isPasswordValid) {
+				const userData = {
+					_id: user._id,
+					userName: user.userName,
+					email: user.email,
+				};
+
+				res.send({
+					message: "Successfully Logged In",
+					alert: true,
+					data: userData,
+				});
+			} else {
+				res.send({
+					message: "Invalid email or password",
+					alert: false,
+				});
+			}
 		} else {
 			res.send({
 				message: "Email is not available, please sign up first",
